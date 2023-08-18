966557384c0e5dd3ca0139d1f1451cf196dbb14d
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-18 16:17:39 -0500

    01-constructor-injection

---
+++ 01-constructor-injection/src/main/java/com/demo/springcoredemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.demo.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component // marks the class as a Java bean(a regular Java class managed by Spring) and makes it available for dependency injection
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.!!!!";
+    }
+}
---
+++ 01-constructor-injection/src/main/java/com/demo/springcoredemo/DemoController.java
@@ -0,0 +1,32 @@
+package com.demo.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired // tells Spring to inject the dependency.
+    // if you only have one constructor then @Autowired is optional
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
+
+/*
+            /dailyWorkout ->              getDailyWorkout() ->
+ Web browser                DemoController                    Coach
+          <- "Practice fast..."                          <- "Practice fast..."
+ */
+
+// Web browser calls dailyworkout -> talks to our DemoController -> DemoController calls the Coach to getDailyWorkout

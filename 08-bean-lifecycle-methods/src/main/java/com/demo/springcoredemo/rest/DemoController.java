45c6d9cdc99e4ebea86bacbf6bb959fef3ed6635
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-20 13:59:42 -0500

    08-bean-lifecycle-methods

---
+++ 08-bean-lifecycle-methods/src/main/java/com/demo/springcoredemo/rest/DemoController.java
@@ -0,0 +1,29 @@
+package com.demo.springcoredemo.rest;
+
+import com.demo.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // constructor injection
+    // Default scope is Singleton meaning that there is only one instance. All dependency injections for the bean will reference the same bean.
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach
+    ) { // you can name this method anything you want. What matters is the @Autowired annotation
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
